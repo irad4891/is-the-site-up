2c2a694262c19e6a7f46ce71d397b2c134ec2bce
Ranjit Singh <rs4891@outlook.com>
2023-12-11 14:30:38 +0530

    First commit

---
+++ src/main/java/in/rstech/isthesiteup/controllers/UrlCheckController.java
@@ -0,0 +1,43 @@
+package in.rstech.isthesiteup.controllers;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class UrlCheckController {
+
+    private final String SITE_IS_UP = "Site is up!";
+    private final String SITE_IS_DOWN = "Site is down!";
+    private final String WRONGURL = "Url is incorrect!";
+    
+
+    @GetMapping("/check")
+    public String getUrlStatuString(@RequestParam String param) {
+
+        try {
+            URL obj = new URL(param);
+            HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int respcode = conn.getResponseCode()/100;
+            if(respcode != 2 && respcode != 3) {
+                return SITE_IS_DOWN;
+            }
+            else {
+                return SITE_IS_UP;
+            }
+        } catch (MalformedURLException e) {
+            return WRONGURL;
+        } catch (IOException e) {
+            return SITE_IS_DOWN;
+        }
+    }
+
+}
---
+++ src/test/java/in/rstech/isthesiteup/IsTheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rstech.isthesiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
